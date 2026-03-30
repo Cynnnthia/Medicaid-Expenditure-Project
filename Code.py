fbc2e84acc8d0bc53099d18a98de9ddffc1781b7
Cynnnthia <cynyang@ucdavis.edu>
2026-03-29 20:07:18 -0700

    Refactor file path generation for Excel files

--- Code.py
+++ Code.py
@@ -4,19 +4,7 @@ years = range(2013, 2025)
 data = []
 
 for year in years:
-    file_path = f"FY_2013_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2014_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2015_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2016_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2017_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2018_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2019_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2020_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2021_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2022_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2023_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2024_MFCU_Statistical_Chart.xlsx"
-    file_path = f"FY_2025_MFCU_Statistical_Chart.xlsx"
+    f"../data/FY_{year}_MFCU_Statistical_Chart.xlsx"
     
     df = pd.read_excel(file_path)
     
